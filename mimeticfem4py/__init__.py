49d4ba252a0f79de8a58fa4f915ce43a776435cc
Artur Palha <artur.palha@gmail.com>
2022-04-24 19:58:52 +0200

    Adds first commit

---
+++ mimeticfem4py/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about mimeticfem4py"""
+import logging
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Artur Palha"
+__email__ = "a.palha@esciencecenter.nl"
+__version__ = "0.1.0"
